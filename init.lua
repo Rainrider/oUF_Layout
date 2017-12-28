fc682317d4eefd83f6f66a490dc9489843ead55e
Rainrider <Rainrider@users.noreply.github.com>
2017-12-28 21:43:19 +0100

    Create an entry point
    Verify oUF version.
    Add debug capability.

---
+++ init.lua
@@ -0,0 +1,14 @@
+local addonName, ns = ...
+
+local oUFVersion = GetAddOnMetadata('oUF', 'version')
+if(not oUFVersion:find('project%-version')) then
+	local major, minor, rev = strsplit('.', oUFVersion)
+	oUFVersion = major * 1000 + minor * 100 + rev
+
+	assert(oUFVersion >= 7000, 'oUF Layout requires oUF version >= 7.0.0')
+end
+
+ns.Debug = function() end
+if(AdiDebug) then
+	ns.Debug = AdiDebug:Embed({}, addonName)
+end
