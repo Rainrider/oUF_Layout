b3cd121ad8004c1bb5964ad197f3a1a24022467e
Rainrider <Rainrider@users.noreply.github.com>
2020-10-05 20:38:40 +0200

    power: use a custom GetDisplayPower
    The default covers group members only.

--- elements/power.lua
+++ elements/power.lua
@@ -1,5 +1,16 @@
 local _, ns = ...
 
+local ALTERNATE_POWER_INDEX = Enum.PowerType.Alternate or 10
+
+local function GetDisplayPower(element)
+	local unit = element.__owner.unit
+	local barInfo = GetUnitPowerBarInfo(unit)
+
+	if barInfo then
+		return ALTERNATE_POWER_INDEX, barInfo.minPower
+	end
+end
+
 function ns.AddPowerBar(self, unit)
 	local power = CreateFrame('StatusBar', nil, self)
 	power:SetStatusBarTexture(ns.assets.TEXTURE)
@@ -19,5 +30,6 @@ function ns.AddPowerBar(self, unit)
 	bg.multiplier = 1/3
 	power.bg = bg
 
+	power.GetDisplayPower = GetDisplayPower
 	self.Power = power
 end
